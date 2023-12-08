3444ce763b958562810ab6324e9532bb5cd28176
Felipe <feltista07123@gmail.com>
2023-12-08 20:22:40 -0300

    resolvendo erros

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/material.dart';
+/*import 'package:flutter/material.dart';
 import 'pages/login_page.dart';
 
 void main() {
@@ -16,6 +16,76 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       home: const LoginPage(),
+    );
+  }
+}*/
+
+
+
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import '../api/locationService.dart';
+import 'package:web_socket_channel/io.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  late LocationService locationService;
+  late IOWebSocketChannel channel;
+
+  @override
+  void initState() {
+    super.initState();
+    initLocationService();
+  }
+
+  Future<void> initLocationService() async {
+    locationService = LocationService();
+    await locationService.init();
+
+    channel = IOWebSocketChannel.connect('ws://10.0.2.2:8080/flutter-app');
+    while (true) {
+      var locationData = await locationService.getLocation();
+      String locationString = '${locationData.latitude}, ${locationData.longitude}';
+      channel.sink.add(locationString);
+      await Future.delayed(const Duration(seconds: 10));
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Text('Obtendo localização...'),
+      ),
     );
   }
 }
